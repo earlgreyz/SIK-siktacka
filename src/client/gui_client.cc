19f1b2ee047c996b9978d85cf4060878efcd88cc
Mikołaj Walczak <mikiwalczak@gmail.com>
2017-06-09 15:57:53 +0200

    Refactor GUI Client

--- src/client/gui_client.cc
+++ src/client/gui_client.cc
@@ -9,43 +9,41 @@ using namespace sikclient;
 
 
 GuiClient::GuiClient(int sock, const std::string &host, network::port_t port)
-        : sock(sock) {
+        : sock(sock), direction(siktacka::direction_t::NONE) {
     connect_to_gui(host, port);
-    direction = siktacka::direction_t::NONE;
 }
 
 void GuiClient::connect_to_gui(const std::string &host, network::port_t port) {
-    addrinfo addr_hints;
-    addrinfo *addr_result;
-    memset(&addr_hints, 0, sizeof(struct addrinfo));
-    addr_hints.ai_flags = 0;
-    addr_hints.ai_family = AF_INET;
-    addr_hints.ai_socktype = SOCK_STREAM;
-    addr_hints.ai_protocol = IPPROTO_TCP;
+    addrinfo hints, *result;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_flags = 0;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
     std::string port_str = boost::lexical_cast<std::string>(port);
 
-    if (getaddrinfo(host.c_str(), port_str.c_str(), &addr_hints, &addr_result)
-        != 0) {
-        throw std::invalid_argument("Error getting gui address");
+    if (getaddrinfo(host.c_str(), port_str.c_str(), &hints, &result) != 0) {
+        throw std::invalid_argument("Error getting GUI address");
     }
 
-    if (connect(sock, addr_result->ai_addr, addr_result->ai_addrlen) != 0) {
-        throw std::runtime_error("Error connecting to gui");
+    if (connect(sock, result->ai_addr, result->ai_addrlen) != 0) {
+        throw gui_error();
     }
-    freeaddrinfo(addr_result);
+    freeaddrinfo(result);
 }
 
 void GuiClient::send_event(const std::string &event) {
     if (send(sock, event.data(), event.length(), 0) < 0) {
-        throw std::runtime_error("GUI Disconnected");
+        throw gui_error();
     }
 }
 
 void GuiClient::receive_event() {
     network::buffer_t buffer(16u, 0);
-    char * data = const_cast<char *>(buffer.data());
+    char *data = const_cast<char *>(buffer.data());
     if (read(sock, data, buffer.size() - 1) <= 0) {
-        throw std::runtime_error("GUI Disconnected");
+        throw gui_error();
     }
 
     std::string action = buffer.data();
@@ -65,8 +63,8 @@ siktacka::direction_t GuiClient::get_direction() const noexcept {
     return direction;
 }
 
-void GuiClient::deselect(siktacka::direction_t deselect) noexcept {
-    if (direction == deselect) {
+void GuiClient::deselect(siktacka::direction_t deselected) noexcept {
+    if (direction == deselected) {
         direction = siktacka::direction_t::NONE;
     }
 }
--- src/client/gui_client.h
+++ src/client/gui_client.h
@@ -6,24 +6,60 @@
 #include "../siktacka/events/event.h"
 
 namespace sikclient {
+
+    class gui_error : std::exception {
+        const char* what() const noexcept override {
+            return "GUI Connection error";
+        }
+    };
+
     class GuiClient {
     private:
+        /// GUI Server TCP socket
         int sock;
+        /// Client current direction
         siktacka::direction_t direction;
 
     public:
-        GuiClient(int sock, const std::string &name, network::port_t port);
+        /**
+         * Constructs new GUI Client.
+         * @param sock GUI Server TCP socket.
+         * @param host GUI Server hostname/ip.
+         * @param port GUI Server port.
+         */
+        GuiClient(int sock, const std::string &host, network::port_t port);
 
+        /**
+         * Sends event to the GUI.
+         * @param event event serialized to GUI readable format.
+         */
         void send_event(const std::string &event);
 
+        /**
+         * Receives event from GUI and changes direction depending on received
+         * event.
+         */
         void receive_event();
 
+        /**
+         * Returns client current direction.
+         * @return direction.
+         */
         siktacka::direction_t get_direction() const noexcept;
 
     private:
-        void connect_to_gui(const std::string &name, network::port_t port);
+        /**
+         * Binds socket to the GUI Server.
+         * @param host GUI Server hostname/ip.
+         * @param port GUI Server port.
+         */
+        void connect_to_gui(const std::string &host, network::port_t port);
 
-        void deselect(siktacka::direction_t deselect) noexcept;
+        /**
+         * Deselects direction if matches currently selected one.
+         * @param deselected direction.
+         */
+        void deselect(siktacka::direction_t deselected) noexcept;
     };
 }
 
