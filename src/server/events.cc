f91a59ccb54aee74478a89bb5156ceaa0e65f85b
Mikołaj Walczak <mikiwalczak@gmail.com>
2017-06-10 15:36:39 +0200

    Optimize event selection algorithm

--- src/server/events.cc
+++ src/server/events.cc
@@ -9,11 +9,8 @@ void Events::add_event(std::shared_ptr<siktacka::Event> event) {
 std::queue<std::shared_ptr<siktacka::Event>>
 Events::get_events(siktacka::event_no_t next_event) const noexcept {
     std::queue<std::shared_ptr<siktacka::Event>> message_events;
-    // TODO: binsearch
-    for (std::size_t i = 0; i < events.size(); i++) {
-        if (events[i]->get_event_no() >= next_event) {
-            message_events.push(events[i]);
-        }
+    for (std::size_t i = next_event; i < events.size(); i++) {
+        message_events.push(events[i]);
     }
     return message_events;
 }
